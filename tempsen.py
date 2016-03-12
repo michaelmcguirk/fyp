37e11aca40d64b257dc64ce01e994a3e7f340735
Michael <michael.mcguirk@mydit.ie>
2016-03-12 12:15:37 +0000

    target temp in tempsen

--- tempsen.py
+++ tempsen.py
@@ -1,4 +1,5 @@
 # Michael McGuirk - D13123389 - DT228/4 - FYP
+# 13/03/16
 import os
 import glob
 import time
@@ -27,6 +28,10 @@ cursor = db.cursor()
 cursor.execute("SELECT VERSION()")
 temp_max = 20.00
 temp_min = 15.00
+
+max_temp = 0
+min_temp = 0
+target_temp = 0
 
 # Read raw temperature data from the sensor
 # Open the w1_slave file and read the data.
@@ -63,7 +68,6 @@ def read_user_temp():
 
 
 while True:
-	#currentTime = datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p")
 	currentTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 	
 	print(currentTime)
@@ -74,6 +78,12 @@ while True:
 		temp_low = user_temps[0]
 		temp_high = user_temps[1]
 		batch_id = user_temps[2]
+
+		desired_mean_temp = (temp_low + temp_high) / 2
+		if target_temp == 0:
+			target_temp = desired_mean_temp
+
+
 		insert_temps = """INSERT INTO temps(tempc,tempf,timestp, batch_id_id)VALUES(%s, %s, %s, %s)"""
 		update_current_temp = """UPDATE current_temp SET tempc=%s,tempf=%s,timestp=%s where temp_id=1"""
 		
@@ -87,13 +97,52 @@ while True:
 
 	# Adjust relay within temperature range
 	temp_c = read_temp()[0]
-	if temp_c > temp_high:
+	print "Target temperature: " + str(target_temp)
+
+	if temp_c > target_temp:
 		G.output(2,G.LOW)
 		print "Relay is off"
-	elif temp_c <= temp_high:
+
+	elif temp_c < target_temp:
 		G.output(2,G.HIGH)
 		print "Relay is now on"
 	
 	# Wait 60 seconds before taking the next reading.
 	time.sleep(60)
+
+
+# while True:
+# 	#currentTime = datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p")
+# 	currentTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+	
+# 	print(currentTime)
+# 	print(read_temp())
+# 	try:
+# 		db.ping(True)
+# 		user_temps = read_user_temp()
+# 		temp_low = user_temps[0]
+# 		temp_high = user_temps[1]
+# 		batch_id = user_temps[2]
+# 		insert_temps = """INSERT INTO temps(tempc,tempf,timestp, batch_id_id)VALUES(%s, %s, %s, %s)"""
+# 		update_current_temp = """UPDATE current_temp SET tempc=%s,tempf=%s,timestp=%s where temp_id=1"""
+		
+# 		cursor.execute(insert_temps, (read_temp()[0],read_temp()[1],currentTime,batch_id))
+
+# 		cursor.execute(update_current_temp, (read_temp()[0],read_temp()[1],currentTime))
+# 		db.commit()
+		
+# 	except MySQLdb.Error, e:
+# 		print "MySQL Error: %s" % str(e)
+
+# 	# Adjust relay within temperature range
+# 	temp_c = read_temp()[0]
+# 	if temp_c > temp_high:
+# 		G.output(2,G.LOW)
+# 		print "Relay is off"
+# 	elif temp_c <= temp_high:
+# 		G.output(2,G.HIGH)
+# 		print "Relay is now on"
+	
+# 	# Wait 60 seconds before taking the next reading.
+# 	time.sleep(60)
 
