7220a3b82eda975ad7e1149902269218de70a092
Michael McGuirk <michael.mcguirk@mydit.ie>
2016-03-17 20:23:35 +0000

    more testing

--- temps/test_models.py
+++ temps/test_models.py
@@ -1,20 +1,39 @@
 from django.test import Client
 import unittest
+from django import test
 from django.contrib.auth.models import User
-from .models import Temps, Batch, CurrentTemp
+from .models import Temps, Batch, CurrentTemp, UserBatchSettings
 
-class SimpleTest(unittest.TestCase):
+class SimpleTest(test.TestCase):
     
     def setUp(self):
         self.user = User.objects.create_user(username = "TestingUser", password="password", email="mail@mail.com")
         self.batch = Batch.objects.create(batch_name="TestingBatch")
         self.temps = Temps.objects.create(batch_id = self.batch, tempc = 1, tempf = 1, timestp = "2016-03-16 00:00:00", seq_no = 1)
+        self.ct = CurrentTemp.objects.create(temp_id=1,tempc=2,tempf=2,timestp="2016-03-16 00:00:00", 
+            temp_high_c=2,temp_low_c=1,current_batch_id=self.batch)
+        self.settings = UserBatchSettings.objects.create(user_id = self.user,def_temp_low=1,def_temp_high=1)
+
 
     def tearDown(self):
-        del self.user
+        self.user.delete()
         del self.batch
         del self.temps
+        del self.ct
+        del self.settings
 
-    def test_temps(self):
+    def test_batch(self):
         self.batch = Batch.objects.get(batch_name="TestingBatch")
         self.assertEqual(self.batch.batch_name,"TestingBatch")
+
+    def test_temps(self):
+        self.temps = Temps.objects.get(timestp = "2016-03-16 00:00:00")
+        #self.assertEqual(self.temps.timestp, "2016-03-16 00:00:00")
+
+    def test_ct(self):
+        self.ct = CurrentTemp.objects.get(timestp = "2016-03-16 00:00:00")
+        #self.assertEqual(self.ct.timestp, "2016-03-16 00:00:00")
+
+    def test_settings(self):
+        self.settings = UserBatchSettings.objects.get(user_id = self.user)
+        self.assertEqual(self.settings.user_id, self.user)
--- temps/test_views.py
+++ temps/test_views.py
@@ -1,29 +1,43 @@
 from django.test import Client
 import unittest
+from django import test
+from .models import UserBatchSettings, Batch, CurrentTemp, Temps
 from django.contrib.auth.models import User
 
-class SimpleTest(unittest.TestCase):
+class SimpleTest(test.TestCase):
     
     def setUp(self):
         self.client = Client()
+        self.user = User.objects.create_user(username="test", password="test", email="email@mail.com")
+        self.client.login(username="test", password="test")
+        self.user = User.objects.create_user(username = "TestingUser", password="password", email="mail@mail.com")
+        self.batch = Batch.objects.create(batch_name="TestingBatch")
+        self.temps = Temps.objects.create(batch_id = self.batch, tempc = 1, tempf = 1, timestp = "2016-03-16 00:00:00", seq_no = 1)
+        self.ct = CurrentTemp.objects.create(temp_id=1,tempc=2,tempf=2,timestp="2016-03-16 00:00:00", 
+            temp_high_c=2,temp_low_c=1,current_batch_id=self.batch)
+        self.settings = UserBatchSettings.objects.create(user_id = self.user,def_temp_low=1,def_temp_high=1)
+    
+    def tearDown(self):
+        del self.client
+        #del self.user
 
     def test_index(self):
         response = self.client.get('/temps/')
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, 200)
 
     def test_view_batches(self):
-        response = self.client.get('/temps/view_user_batches/1/')
+        response = self.client.get('/temps/view_user_batches/' + str(self.user.id) + '/')
         self.assertEqual(response.status_code, 302)
 
     def test_view_batch(self):
-    	response = self.client.get('/temps/view_batch/1/')
-    	self.assertEqual(response.status_code, 302)
+    	response = self.client.get('/temps/view_batch/' + str(self.batch.id) + '/')
+    	self.assertEqual(response.status_code, 200)
     	#self.assertEqual(len(response.context['customers']), 5)
 
     def test_edit_batch(self):
-    	response = self.client.get('/temps/edit_batch/1/')
-    	self.assertEqual(response.status_code, 302)
+    	response = self.client.get('/temps/edit_batch/' + str(self.batch.id) + '/')
+    	self.assertEqual(response.status_code, 200)
 
     def test_serve_compare_chart(self):
-    	response = self.client.get('/temps/serve_compare_chart/1/2/')
-    	self.assertEqual(response.status_code, 302)
+    	response = self.client.get('/temps/serve_compare_chart/' + str(self.batch.id) + '/' + str(self.batch.id) + '/')
+    	self.assertEqual(response.status_code, 200)
